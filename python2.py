3de86ccd9b0f159a87e1d38d04e4c4bcc9c1ade0
ShakirahAR <g-10221910@moe-dl.edu.my>
2024-07-03 10:59:32 +0800

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,7 @@
+#panjang = int(input("Masukkan panjang segi empat:"))
+#lebar = int(input ("Masukkan lebar segi empat:"))
+#luas = panjang*lebar
+#print (luas)
+
+
+username1=(input(“Masukkan username:”))
